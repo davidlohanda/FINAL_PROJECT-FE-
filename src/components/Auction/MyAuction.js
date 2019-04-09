b4d18eb832e1b975486e14d35406aa83647f2aaa
David <davidlohanda8@gmail.com>
2019-04-09 19:20:53 +0700

    david

--- src/components/Auction/MyAuction.js
+++ src/components/Auction/MyAuction.js
@@ -5,10 +5,11 @@ import cookie from 'universal-cookie'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 
+
 const Cookie = new cookie()
 
 class MyAuction extends React.Component{
-    state={myAuction : [] , dataEdit : {} , selectedFileImageEdit : null}
+    state={myAuction : [] , dataEdit : {} , selectedFileImageEdit : null , modal : false}
     componentDidMount(){
         this.getMyAuction()
     }
@@ -33,14 +34,61 @@ class MyAuction extends React.Component{
                     <td> <img src = {'http://localhost:2000/' + val.product_image} 
                       width = '50px' alt=""/>  
                     </td>
-                    <td> <input type = 'button' className='btn btn-info'  value='edit' style={{fontSize:'18px', marginTop:'-2px'}}/> </td>
-                    <td> <input type = 'button' className='btn btn-danger' value='delete' style={{fontSize:'18px', marginTop:'-2px'}}/> </td>        
+                    <td> <input type = 'button' className='btn btn-info'  value='edit' onClick={()=>{this.setState({modal : true , dataEdit : val})}} style={{fontSize:'18px', marginTop:'-2px'}}/> </td>
+                    <td> <input type = 'button' className='btn btn-danger' value='delete' onClick={()=>this.onBtnDeleteClick(val)} style={{fontSize:'18px', marginTop:'-2px'}}/> </td>        
               </tr>
             )
         })
         return jsx
     }
+
+    valueHandlerEdit = () => {
+        return this.state.selectedFileImageEdit? this.state.selectedFileImageEdit.name : 'Edit Picture'
+    }
+
+    onChangeHandlerEdit = (e) => {
+        this.setState({selectedFileImageEdit : e.target.files[0]})
+    }
+
+    onBtnSaveClick = () => {
+        var data = {
+            product_name : this.refs.product_nameEdit.value ? this.refs.product_nameEdit.value : this.state.dataEdit.product_name,
+            product_price : this.refs.product_priceEdit.value ? this.refs.product_priceEdit.value : this.state.dataEdit.product_price,
+            add_price : this.refs.add_priceEdit.value ? this.refs.add_priceEdit.value : this.state.dataEdit.add_price,
+            product_desc : this.refs.product_descEdit.value ? this.refs.product_descEdit.value : this.state.dataEdit.product_desc
+        }
+
+        if(this.state.selectedFileImageEdit){
+            var fd = new FormData()
+            fd.append('editImg' , this.state.selectedFileImageEdit)
+            fd.append('editAuction' , JSON.stringify(data))
+            fd.append('imageBefore' , this.state.dataEdit.product_image)
+            axios.put(`http://localhost:2000/auction/editCreateAuction?id=${this.state.dataEdit.id}`, fd)
+            .then((res) => {
+                alert(res.data)
+                this.setState({modal : false})
+                this.getMyAuction()
+            })
+            .catch((err) => console.log(err))
+        }else{
+            axios.put(`http://localhost:2000/auction/editCreateAuction?id=${this.state.dataEdit.id}`, data)
+            .then((res) => {
+                alert(res.data)
+                this.setState({modal : false})
+                this.getMyAuction()
+            })
+            .catch((err) => console.log(err))
+        }
+    }
     
+    onBtnDeleteClick = (val) => {
+        axios.delete(`http://localhost:2000/auction/deleteCreateAuction?id=${val.id}&imageBefore=${val.product_image}`)
+        .then((res)=>{
+            alert(res.data)
+            this.getMyAuction()
+        })
+    }
+
     render(){
         return(
             <div>
@@ -57,34 +105,43 @@ class MyAuction extends React.Component{
                 {this.renderJsx()}
                 </table>
 
+                {/* =============MODAL================= */}
                 <div>
-                    <Modal isOpen={this.state.modal} toggle={() => this.setState({modal:false})} className={this.props.className}>
-                        <ModalHeader toggle={() => this.setState({modal:false})}>Edit Product ~ {this.state.dataEdit.product_name}</ModalHeader>
-                        <ModalBody>
-                        <div className='row'> 
-                            <div className='col-md-3'>
-                            <img src={'http://localhost:4000/' + this.state.dataEdit.product_image} width='100%' alt='broken' />
+                    <Modal isOpen={this.state.modal} toggle={()=>{this.setState({modal : false})}} className={this.props.className}>
+                    <ModalHeader toggle={()=>{this.setState({modal : false})}}>Edit - {this.state.dataEdit.product_name}</ModalHeader>
+                    <ModalBody>
+                    <div className='row'> 
+                        <div className='col-md-3'>
+                            <img src={'http://localhost:2000/' + this.state.dataEdit.product_image} width='100%' alt='broken' />
                             <input type='file' onChange={this.onChangeHandlerEdit} style={{display:'none'}} ref='inputEdit' />
                             <input type='button' value={this.valueHandlerEdit()} className='btn btn-primary' 
-                                    onClick={ () =>this.refs.inputEdit.click()} />
-                            </div>
-                            <div className='col-md-9'> 
-                            <input type='text' className='form-control' 
-                            placeholder={this.state.dataEdit.product_name} ref='namaEdit' />
-                            <input type='number' className='form-control mt-3' 
-                            placeholder={this.state.dataEdit.product_price} ref='hargaEdit' />
-                            </div>
+                            onClick={ () =>this.refs.inputEdit.click()} style={{fontSize:'14px', marginTop:'3px',width:'100%'}} />
                         </div>
-                        </ModalBody>
-                        <ModalFooter>
-                        <Button color="primary" onClick={this.onSaveBtnClick}>Save</Button>{' '}
-                        <Button color="secondary" onClick={() => this.setState({modal:false})}>Cancel</Button>
-                        </ModalFooter>
+                        <div className='col-md-9'>
+                            <label>Product Name</label> 
+                            <input type='text' className='form-control' 
+                            defaultValue={this.state.dataEdit.product_name} ref='product_nameEdit' />
+                            
+                            <label className="mt-3">Product Price</label>
+                            <input type='number' className='form-control' 
+                            defaultValue={this.state.dataEdit.product_price} ref='product_priceEdit' />
+                            
+                            <label className="mt-3">Product Add Price</label>
+                            <input type='number' className='form-control' 
+                            defaultValue={this.state.dataEdit.add_price} ref='add_priceEdit' />
+
+                            <label className="mt-3">Product Description</label>
+                            <textarea className="form-control" defaultValue={this.state.dataEdit.product_desc} id="productDesc" rows={3} ref="product_descEdit" required/>
+                        </div>
+                    </div>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.onBtnSaveClick} style={{fontSize:'18px', marginTop:'-2px'}}>Save</Button>{' '}
+                        <Button color="secondary" onClick={()=>{this.setState({modal : false})}} style={{fontSize:'18px', marginTop:'-2px'}}>Cancel</Button>
+                    </ModalFooter>
                     </Modal>
                 </div>
             </div>
-
-            
         )
     }
 }
